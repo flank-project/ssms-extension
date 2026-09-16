6a70382a946b70e4d1f558106ce958cdccecca60
Angus Mitchell <anguspmitchell@gmail.com>
2026-09-16 20:12:30 +0000

    write the updated powerquery stuff back into file

--- Flank.Excel/ExcelExporter.cs
+++ Flank.Excel/ExcelExporter.cs
@@ -6,13 +6,17 @@ namespace Flank.Excel;
 
 public static class ExcelExporter
 {
-    public static void Generate(string templatePath, string outputPath)
+    public static void Generate(
+        string templatePath,
+        string outputPath,
+        string sql)
     {
         File.Copy(templatePath, outputPath, overwrite: true);
 
-        using var workbook = ZipFile.Open(outputPath, ZipArchiveMode.Read);
+        using var workbook = ZipFile.Open(
+            outputPath,
+            ZipArchiveMode.Update);
 
-        // Find customXml/item1.xml inside the XLSX
         var item = workbook.GetEntry("customXml/item1.xml")
             ?? throw new Exception("Could not find customXml/item1.xml");
 
@@ -23,35 +27,89 @@ public static class ExcelExporter
             xml = XDocument.Load(stream);
         }
 
-        // <DataMashup> contains the Base64-encoded Power Query package
         var dataMashup = xml.Root
             ?? throw new Exception("Could not find DataMashup");
 
-        byte[] mashupBytes = Convert.FromBase64String(dataMashup.Value);
+        byte[] mashupBytes =
+            Convert.FromBase64String(dataMashup.Value);
 
-        // Bytes 4-7 contain the length of the embedded ZIP
-        int zipLength = BitConverter.ToInt32(mashupBytes, 4);
+        int oldZipLength =
+            BitConverter.ToInt32(mashupBytes, 4);
 
-        Console.WriteLine($"Embedded ZIP length: {zipLength}");
-
-        // ZIP begins at byte 8
-        byte[] zipBytes = mashupBytes
+        byte[] oldZipBytes = mashupBytes
             .Skip(8)
-            .Take(zipLength)
+            .Take(oldZipLength)
             .ToArray();
 
-        // Open the embedded ZIP in memory
-        using var memory = new MemoryStream(zipBytes);
-        using var mashupZip = new ZipArchive(memory, ZipArchiveMode.Read);
+        // Everything after the embedded ZIP must be preserved.
+        byte[] trailingBytes = mashupBytes
+            .Skip(8 + oldZipLength)
+            .ToArray();
 
-        var section = mashupZip.GetEntry("Formulas/Section1.m")
-            ?? throw new Exception("Could not find Formulas/Section1.m");
+        // Make a writable copy of the embedded ZIP.
+        using var zipStream = new MemoryStream();
+        zipStream.Write(oldZipBytes);
+        zipStream.Position = 0;
 
-        using var reader = new StreamReader(section.Open(), Encoding.UTF8);
+        using (var mashupZip =
+            new ZipArchive(zipStream, ZipArchiveMode.Update, leaveOpen: true))
+        {
+            var section = mashupZip.GetEntry("Formulas/Section1.m")
+                ?? throw new Exception("Could not find Formulas/Section1.m");
 
-        string mCode = reader.ReadToEnd();
+            string mCode;
 
-        Console.WriteLine("----- Section1.m -----");
-        Console.WriteLine(mCode);
+            using (var reader =
+                new StreamReader(section.Open(), Encoding.UTF8))
+            {
+                mCode = reader.ReadToEnd();
+            }
+
+            // For now, replace our known test SQL.
+            mCode = mCode.Replace(
+                "select * from vehicle_trips",
+                sql);
+
+            // ZIP entries can't be overwritten directly.
+            section.Delete();
+
+            var newSection =
+                mashupZip.CreateEntry("Formulas/Section1.m");
+
+            using var writer =
+                new StreamWriter(newSection.Open(), new UTF8Encoding(false));
+
+            writer.Write(mCode);
+        }
+
+        byte[] newZipBytes = zipStream.ToArray();
+
+        // Rebuild the complete DataMashup binary.
+        using var newMashup = new MemoryStream();
+
+        // Preserve bytes 0-3.
+        newMashup.Write(mashupBytes, 0, 4);
+
+        // Bytes 4-7 = new ZIP length.
+        newMashup.Write(BitConverter.GetBytes(newZipBytes.Length));
+
+        // New embedded ZIP.
+        newMashup.Write(newZipBytes);
+
+        // Preserve everything after it.
+        newMashup.Write(trailingBytes);
+
+        dataMashup.Value =
+            Convert.ToBase64String(newMashup.ToArray());
+
+        // Replace item1.xml in the XLSX.
+        item.Delete();
+
+        var newItem =
+            workbook.CreateEntry("customXml/item1.xml");
+
+        using var xmlStream = newItem.Open();
+
+        xml.Save(xmlStream);
     }
 }
