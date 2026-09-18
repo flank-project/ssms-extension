da5bb2b7461a6e6f672653b12a1116b1e63c4a9c
Angus Mitchell <anguspmitchell@gmail.com>
2026-09-18 17:15:22 +0000

    big rewrite with error handling

--- Flank.SsmsExtension/SqlToExcelPowerQueryCommand.cs
+++ Flank.SsmsExtension/SqlToExcelPowerQueryCommand.cs
@@ -1,207 +1,217 @@
-﻿using Microsoft.VisualStudio.Shell;
+﻿using Flank.Excel;
+using Microsoft.SqlServer.Management.UI.VSIntegration;
+using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 using System;
 using System.ComponentModel.Design;
-using System.Globalization;
-using System.Threading;
 using System.Threading.Tasks;
 using Task = System.Threading.Tasks.Task;
-using Microsoft.SqlServer.Management.UI.VSIntegration;
-using Flank.Excel;
 
 namespace Flank.SsmsExtension
 {
-    /// <summary>
-    /// Command handler
-    /// </summary>
     internal sealed class SqlToExcelPowerQueryCommand
     {
-        /// <summary>
-        /// Command ID.
-        /// </summary>
         public const int CommandId = 0x0100;
 
-        /// <summary>
-        /// Command menu group (command set GUID).
-        /// </summary>
-        public static readonly Guid CommandSet = new Guid("4aefd085-bfeb-4d3d-8199-201b0c86abc0");
+        public static readonly Guid CommandSet =
+            new Guid("4aefd085-bfeb-4d3d-8199-201b0c86abc0");
 
-        /// <summary>
-        /// VS Package that provides this command, not null.
-        /// </summary>
         private readonly AsyncPackage package;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="SqlToExcelPowerQueryCommand"/> class.
-        /// Adds our command handlers for menu (commands must exist in the command table file)
-        /// </summary>
-        /// <param name="package">Owner package, not null.</param>
-        /// <param name="commandService">Command service to add command to, not null.</param>
-        private SqlToExcelPowerQueryCommand(AsyncPackage package, OleMenuCommandService commandService)
+        private SqlToExcelPowerQueryCommand(
+            AsyncPackage package,
+            OleMenuCommandService commandService)
         {
-            this.package = package ?? throw new ArgumentNullException(nameof(package));
-            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+            this.package = package
+                ?? throw new ArgumentNullException(nameof(package));
 
-            var menuCommandID = new CommandID(CommandSet, CommandId);
-            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService = commandService
+                ?? throw new ArgumentNullException(nameof(commandService));
+
+            var commandId = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(Execute, commandId);
+
             commandService.AddCommand(menuItem);
         }
 
-        /// <summary>
-        /// Gets the instance of the command.
-        /// </summary>
-        public static SqlToExcelPowerQueryCommand Instance
-        {
-            get;
-            private set;
-        }
+        public static SqlToExcelPowerQueryCommand Instance { get; private set; }
 
-        /// <summary>
-        /// Gets the service provider from the owner package.
-        /// </summary>
-        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
-        {
-            get
-            {
-                return this.package;
-            }
-        }
-
-        /// <summary>
-        /// Initializes the singleton instance of the command.
-        /// </summary>
-        /// <param name="package">Owner package, not null.</param>
         public static async Task InitializeAsync(AsyncPackage package)
         {
-            // Switch to the main thread - the call to AddCommand in Command1's constructor requires
-            // the UI thread.
-            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(
+                package.DisposalToken);
 
-            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
-            Instance = new SqlToExcelPowerQueryCommand(package, commandService);
+            var commandService =
+                await package.GetServiceAsync(typeof(IMenuCommandService))
+                as OleMenuCommandService;
+
+            Instance = new SqlToExcelPowerQueryCommand(
+                package,
+                commandService);
         }
 
-        /// <summary>
-        /// This function is the callback used to execute the command when the menu item is clicked.
-        /// See the constructor to see how the menu item is associated with this function using
-        /// OleMenuCommandService service and MenuCommand class.
-        /// </summary>
-        /// <param name="sender">Event sender.</param>
-        /// <param name="e">Event args.</param>
         private void Execute(object sender, EventArgs e)
         {
             ThreadHelper.ThrowIfNotOnUIThread();
 
-            var dte = Package.GetGlobalService(typeof(EnvDTE.DTE)) as EnvDTE.DTE;
+            string step = "Reading query";
+            string server = null;
+            string database = null;
+            string outputPath = null;
+
+            try
+            {
+                var dte = Package.GetGlobalService(typeof(EnvDTE.DTE))
+                    as EnvDTE.DTE;
+
+                if (dte == null)
+                    throw new Exception("Could not access the SSMS editor.");
+
+                var doc = dte.ActiveDocument;
+
+                if (doc == null)
+                {
+                    ShowMessage(
+                        "Open a SQL query window first.",
+                        OLEMSGICON.OLEMSGICON_INFO);
+
+                    return;
+                }
+
+                var selection = doc.Selection as EnvDTE.TextSelection;
+
+                if (selection == null)
+                    throw new Exception("Could not read the current SQL editor.");
+
+                string sql;
+
+                if (!string.IsNullOrWhiteSpace(selection.Text))
+                {
+                    sql = selection.Text;
+                }
+                else
+                {
+                    var textDoc = doc.Object("TextDocument")
+                        as EnvDTE.TextDocument;
+
+                    if (textDoc == null)
+                        throw new Exception("Could not read the current SQL document.");
+
+                    var start = textDoc.StartPoint.CreateEditPoint();
+                    sql = start.GetText(textDoc.EndPoint);
+                }
+
+                if (string.IsNullOrWhiteSpace(sql))
+                {
+                    ShowMessage(
+                        "The current query is empty.",
+                        OLEMSGICON.OLEMSGICON_INFO);
+
+                    return;
+                }
+
+                step = "Reading SQL Server connection";
+
+                var scriptFactory = ServiceCache.ScriptFactory;
+
+                if (scriptFactory == null)
+                    throw new Exception("Could not access the SSMS connection service.");
+
+                var connection = scriptFactory.CurrentlyActiveWndConnectionInfo;
+                var info = connection.UIConnectionInfo;
+
+                if (info == null)
+                    throw new Exception("No active SQL Server connection was found.");
+                server = info.ServerName;
+                database = info.AdvancedOptions["DATABASE"];
+
+                if (string.IsNullOrWhiteSpace(server))
+                    throw new Exception("Could not determine the SQL Server name.");
+
+                if (string.IsNullOrWhiteSpace(database))
+                    throw new Exception("Could not determine the database name.");
+
+                step = "Choosing output file";
+
+                using (var dialog = new System.Windows.Forms.SaveFileDialog
+                {
+                    Filter = "Excel Workbook (*.xlsx)|*.xlsx",
+                    DefaultExt = "xlsx",
+                    AddExtension = true,
+                    FileName = "report.xlsx",
+                    Title = "Save as refreshable Excel"
+                })
+                {
+                    if (dialog.ShowDialog() !=
+                        System.Windows.Forms.DialogResult.OK)
+                    {
+                        return;
+                    }
+
+                    outputPath = dialog.FileName;
+                }
+
+                step = "Creating Excel workbook";
+
+                ExcelExporter.Generate(
+                    outputPath,
+                    server,
+                    database,
+                    sql);
+
+                step = "Opening Excel workbook";
+
+                System.Diagnostics.Process.Start(outputPath);
+            }
+            catch (Exception ex)
+            {
+                ShowFailure(
+                    step,
+                    ex,
+                    server,
+                    database,
+                    outputPath);
+            }
+        }
+
+        private void ShowFailure(
+            string step,
+            Exception exception,
+            string server,
+            string database,
+            string outputPath)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            var message =
+                "Flank couldn't create the refreshable Excel workbook.\n\n" +
+                $"Step: {step}\n" +
+                $"Error: {exception.Message}\n\n" +
+                $"Server: {server ?? "(unknown)"}\n" +
+                $"Database: {database ?? "(unknown)"}\n" +
+                $"Output: {outputPath ?? "(not chosen yet)"}\n\n" +
+                "Please screenshot this message when reporting the issue.";
+
+            ShowMessage(
+                message,
+                OLEMSGICON.OLEMSGICON_CRITICAL,
+                "Flank Error");
+        }
+
+        private void ShowMessage(
+            string message,
+            OLEMSGICON icon,
+            string title = "Flank")
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
 
             VsShellUtilities.ShowMessageBox(
-                this.package,
-                dte == null ? "DTE is NULL" : "DTE exists!",
-                "Flank Debug",
-                OLEMSGICON.OLEMSGICON_INFO,
+                package,
+                message,
+                title,
+                icon,
                 OLEMSGBUTTON.OLEMSGBUTTON_OK,
                 OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
-
-            var doc = dte.ActiveDocument;
-
-            if (doc == null)
-            {
-                VsShellUtilities.ShowMessageBox(
-                    this.package,
-                    "Open a query window first.",
-                    "Flank",
-                    OLEMSGICON.OLEMSGICON_INFO,
-                    OLEMSGBUTTON.OLEMSGBUTTON_OK,
-                    OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
-
-                return;
-            }
-
-            var selection = (EnvDTE.TextSelection)doc.Selection;
-
-            string sql;
-
-            if (!string.IsNullOrWhiteSpace(selection.Text))
-            {
-                sql = selection.Text;
-            }
-            else
-            {
-                var textDoc = (EnvDTE.TextDocument)
-                    doc.Object("TextDocument");
-
-                var start = textDoc.StartPoint.CreateEditPoint();
-                sql = start.GetText(textDoc.EndPoint);
-            }
-
-
-            VsShellUtilities.ShowMessageBox(
-                this.package,
-                sql,
-                "Flank SQL",
-                OLEMSGICON.OLEMSGICON_INFO,
-                OLEMSGBUTTON.OLEMSGBUTTON_OK,
-                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
-
-            var scriptFactory = ServiceCache.ScriptFactory;
-            var connection = scriptFactory.CurrentlyActiveWndConnectionInfo;
-            var info = connection.UIConnectionInfo;
-            if (info == null)
-            {
-                VsShellUtilities.ShowMessageBox(
-                    this.package,
-                    "No active connection.",
-                    "Flank",
-                    OLEMSGICON.OLEMSGICON_INFO,
-                    OLEMSGBUTTON.OLEMSGBUTTON_OK,
-                    OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
-
-                return;
-            }
-            var details =
-                $"ServerName: {info.ServerName}\n" +
-                $"DisplayName: {info.DisplayName}\n" +
-                $"OtherParams: {info.OtherParams}\n\n" +
-                "AdvancedOptions:\n";
-
-            foreach (string key in info.AdvancedOptions.AllKeys)
-            {
-                details += $"{key} = {info.AdvancedOptions[key]}\n";
-            }
-
-            VsShellUtilities.ShowMessageBox(
-                this.package,
-                details,
-                "Flank Connection",
-                OLEMSGICON.OLEMSGICON_INFO,
-                OLEMSGBUTTON.OLEMSGBUTTON_OK,
-                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
-
-            string server = info.ServerName;
-            string database = info.AdvancedOptions["DATABASE"];
-
-            using var dialog = new System.Windows.Forms.SaveFileDialog
-            {
-                Filter = "Excel Workbook (*.xlsx)|*.xlsx",
-                DefaultExt = "xlsx",
-                AddExtension = true,
-                FileName = "report.xlsx",
-                Title = "Save as refreshable Excel"
-            };
-
-            if (dialog.ShowDialog() != System.Windows.Forms.DialogResult.OK)
-            {
-                return;
-            }
-
-            string outputPath = dialog.FileName;
-
-            ExcelExporter.Generate(
-                outputPath,
-                sql,
-                server,
-                database);
-            System.Diagnostics.Process.Start(outputPath);
         }
     }
 }
